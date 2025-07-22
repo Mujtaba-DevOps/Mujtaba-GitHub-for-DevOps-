14ba8d699865e934460467ed11ac1869cd90e954
Mujtaba Anwar Shaikh <shaikhmujtaba072@gmail.com>
2025-07-22 21:04:29 +0530

    added test

--- testing.py
+++ testing.py
@@ -5,6 +5,11 @@ def add(a, b):
     return a + b
 
 
-if __name__ == "__main__":
+def main():
+    """Main function to execute addition."""
     output_result = add(3, 5)
     print(f"The sum is: {output_result}")
+
+
+if __name__ == "__main__":
+    main()
